558171f5cd61e62495293dbd394bac069eda5361
Patrick Clément-Bonhomme <patrick.cb@gmail.com>
2017-09-15 20:55:06 -0400

    Added function for reading text files.

---
+++ src/File.h
@@ -0,0 +1,15 @@
+#ifndef MOONSHINE_FILE_H
+#define MOONSHINE_FILE_H
+
+#include <string>
+
+namespace Moonshine::File
+{
+
+/// Reads a text file into a string.
+static std::string readTextFile(const std::string& filename);
+
+}
+
+#endif //MOONSHINE_SHADER_H
+
